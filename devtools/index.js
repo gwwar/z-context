4e0a625aff4b8455b2a4a60c46b8e7318c362026
Kerry Liu <kerryliu@gmail.com>
2017-04-27 17:05:21 -0700

    Add new z-index rules

--- devtools/index.js
+++ devtools/index.js
@@ -3,29 +3,77 @@ function zContext() {
 		//Also see: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context
 		getClosestStackingContext = function( nodeOrObject ) {
 			var node = nodeOrObject.node || nodeOrObject;
+
+			//the root element (HTML),
 			if( ! node || node.nodeName === 'HTML' ) {
 				return { node: document.documentElement, reason: 'root' };
 			}
+
 			var computedStyle = getComputedStyle( node );
+
+			// position: fixed
 			if ( computedStyle.position === 'fixed' ) {
 				return { node: node, reason: 'position: fixed' };
-			} else if ( computedStyle.zIndex !== 'auto' && computedStyle.position !== 'static' ) {
-				return { node: node, reason: 'position: ' + computedStyle.position + '; z-index: ' + computedStyle.zIndex };
-			} else if ( computedStyle.opacity !== '1' ) {
-				return { node: node, reason: 'opacity: ' + computedStyle.opacity };
-			} else if ( computedStyle.transform !== 'none' ) {
-				return { node: node, reason: 'transform: ' + computedStyle.transform };
-			} else if ( computedStyle.mixBlendMode !== 'normal' ) {
-				return { node: node, reason: 'mixBlendMode: ' + computedStyle.mixBlendMode };
-			} else if ( computedStyle.filter !== 'none' ) {
-				return { node: node, reason: 'filter: ' + computedStyle.filter };
-			} else if ( computedStyle.isolation === 'isolate' ) {
-				return { node: node, reason: 'isolation: ' + computedStyle.isolation };
-			} else if ( computedStyle.webkitOverflowScrolling === 'touch' ) {
-				return { node: node, reason: '-webkit-overflow-scrolling: touch' };
-			} else {
-				return getClosestStackingContext( { node: node.parentNode, reason: 'not a stacking context' } );
 			}
+
+			// positioned (absolutely or relatively) with a z-index value other than "auto",
+			if ( computedStyle.zIndex !== 'auto' && computedStyle.position !== 'static' ) {
+				return { node: node, reason: 'position: ' + computedStyle.position + '; z-index: ' + computedStyle.zIndex };
+			}
+
+			// elements with an opacity value less than 1.
+			if ( computedStyle.opacity !== '1' ) {
+				return { node: node, reason: 'opacity: ' + computedStyle.opacity };
+			}
+
+			// elements with a transform value other than "none"
+			if ( computedStyle.transform !== 'none' ) {
+				return { node: node, reason: 'transform: ' + computedStyle.transform };
+			}
+
+			// elements with a mix-blend-mode value other than "normal"
+			if ( computedStyle.mixBlendMode !== 'normal' ) {
+				return { node: node, reason: 'mixBlendMode: ' + computedStyle.mixBlendMode };
+			}
+
+			// elements with a filter value other than "none"
+			if ( computedStyle.filter !== 'none' ) {
+				return { node: node, reason: 'filter: ' + computedStyle.filter };
+			}
+
+			// elements with a perspective value other than "none"
+			if ( computedStyle.perspective !== 'none' ) {
+				return { node: node, reason: 'perspective: ' + computedStyle.perspective };
+			}
+
+			// elements with isolation set to "isolate"
+			if ( computedStyle.isolation === 'isolate' ) {
+				return { node: node, reason: 'isolation: ' + computedStyle.isolation };
+			}
+
+			// specifying any attribute above in will-change even if you don't specify values for these attributes directly
+			if( computedStyle.willChange !== 'auto' ) {
+				return { node: node, reason: 'willChange: ' + computedStyle.willChange };
+			}
+
+			// elements with -webkit-overflow-scrolling set to "touch"
+			if ( computedStyle.webkitOverflowScrolling === 'touch' ) {
+				return { node: node, reason: '-webkit-overflow-scrolling: touch' };
+			}
+
+			// a flex item with a z-index value other than "auto", that is the parent element display: flex|inline-flex,
+			if ( computedStyle.zIndex !== 'auto' ) {
+				var parentStyle = getComputedStyle( node.parentNode );
+				if ( parentStyle.display === 'flex' || parentStyle.display === 'inline-flex' ) {
+					return {
+						node: node,
+						reason: 'display: ' + computedStyle.display + '; z-index: ' + computedStyle.zIndex
+					};
+				}
+			}
+
+			return getClosestStackingContext( { node: node.parentNode, reason: 'not a stacking context' } );
+
 		},
 		shallowCopy = function( data ) {
 			var props = Object.getOwnPropertyNames( data );
